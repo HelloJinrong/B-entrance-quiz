5e113991588fdff1bc0351780061d651bace581d
jinrong <hjr2027@163.com>
2020-10-27 22:51:42 +0800

    [hejirnong] fix: add student

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/StudentsService.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/StudentsService.java
@@ -12,21 +12,31 @@ import java.util.List;
 
 @Service
 public class StudentsService {
-    List<Students> studentsList;
+    List<Students> studentsList=init() ;
+    List<Students> newStudentsList = new ArrayList();
+    List<Students> AllStudentsList;
     List<Group> groupList;
     int studentNum=15;
-    String[] studentsName = {
-            "成吉思汗", "鲁班七号", "太乙真人", "钟无艳", "花木兰", "雅典娜", "芈月", "白起", "刘婵",
-            "庄周", "马超", "刘备", "哪吒", "大乔", "蔡文姬"
-    };
 
-    public ResponseEntity<List<Students>> getStudents() {
-        studentsList = new ArrayList<>();
+
+    private List<Students> init(){
+        List<Students> temp=new ArrayList<>();
+        String[] studentsName = {
+                "成吉思汗", "鲁班七号", "太乙真人", "钟无艳", "花木兰", "雅典娜", "芈月", "白起", "刘婵",
+                "庄周", "马超", "刘备", "哪吒", "大乔", "蔡文姬"
+        };
         for (int i = 0; i < 15; i++) {
             Students students = new Students(i + 1, studentsName[i]);
-            studentsList.add(students);
+            temp.add(students);
         }
-        return ResponseEntity.ok(studentsList);
+        return temp;
+    }
+
+    public ResponseEntity<List<Students>> getStudents() {
+        AllStudentsList = new ArrayList();
+        AllStudentsList.addAll(studentsList);
+        AllStudentsList.addAll(newStudentsList);
+        return ResponseEntity.ok(AllStudentsList);
     }
 
     private void initGroup(){
@@ -34,18 +44,18 @@ public class StudentsService {
     }
     public ResponseEntity<List<Group>> getGroups() {
 
-        Collections.shuffle(studentsList);
-        int GroupSize = studentsList.size()/6;
-        int restNum = studentsList.size()%6;
+        Collections.shuffle(AllStudentsList);
+        int GroupSize = AllStudentsList.size()/6;
+        int restNum = AllStudentsList.size()%6;
         initGroup();
         for (int i = 0; i < 6; i++) {
             boolean flag=true;
             List<Students> tmp = new ArrayList<>();
             for (int k = 0; k < GroupSize ; k++) {
-                tmp.add(studentsList.get(i *  GroupSize + k));
+                tmp.add(AllStudentsList.get(i *  GroupSize + k));
                 if(restNum>0&&flag==true)
                 {
-                    tmp.add(studentsList.get(GroupSize*6 +i));
+                    tmp.add(AllStudentsList.get(GroupSize*6 +i));
                     restNum--;
                     flag=false;
                 }
@@ -59,10 +69,10 @@ public class StudentsService {
         return ResponseEntity.ok(groupList);
     }
 
-    public ResponseEntity addStudents(Students newstudent){
-        Students students = new Students(studentNum + 1, newstudent.getName());
+    public ResponseEntity addStudents(Students s ){
+        Students students = new Students(studentNum + 1,s.getName());
         studentNum++;
-        studentsList.add(students);
+        newStudentsList.add(students);
         return ResponseEntity.ok().build();
     }
 
